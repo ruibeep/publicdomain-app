a958a1ac19ddc00be9f17b8f79a1f434a82aebf1
ruibeep <rui@intk.com>
2025-04-14 15:00:59 +0200

    refactor: Remove commented-out code iAdded a con secret

--- app/cron/quarterHourly/route.ts
+++ app/cron/quarterHourly/route.ts
@@ -45,13 +45,13 @@ async function publishScheduledPosts(platforms: string[], dbclient) {
 
 
 
-export async function GET(request: NextRequest) {/*
+export async function GET(request: NextRequest) {
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
         return new Response('Unauthorized', {
             status: 401,
         });
-    }*/
+    }
 
     const client = createSocialMediaClient('X');
     const databaseClient = await db.connect();
